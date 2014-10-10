3b51d7f3f959d3a0eec1063352a6070d8a9ec2a1
Paul De Silva <p.desilva@klab.com>
2014-10-10 16:42:28 +0800

    Chap5 sample exercises

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,10 +1,11 @@
 package com.bignerdranch.android.geoquiz;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,15 +20,17 @@ public class QuizActivity extends ActionBarActivity {
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
+	private Button mCheatButton;
+	private boolean mIsCheater;
+	
 	private TextView mQuestionTextView;
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[]{
-					new TrueFalse(R.string.question_oceans, true),
-					new TrueFalse(R.string.question_mideast, false),
-					new TrueFalse(R.string.question_africa, false),
-					new TrueFalse(R.string.question_americas, true),
-					new TrueFalse(R.string.question_asia, true)
-			};
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true),};
 			
 	private int mCurrentIndex = 0;
 			
@@ -42,10 +45,14 @@ public class QuizActivity extends ActionBarActivity {
 		
 		int messageResId = 0;
 		
-		if (userPressedTrue == answerIsTrue){
-			messageResId = R.string.correct_toast;
-		} else {
-			messageResId = R.string.incorrect_toast;
+		if(mIsCheater){
+			messageResId = R.string.judgement_toast;
+		}else{
+			if (userPressedTrue == answerIsTrue){
+				messageResId = R.string.correct_toast;
+			} else {
+				messageResId = R.string.incorrect_toast;
+			}
 		}
 		
 		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
@@ -81,6 +88,7 @@ public class QuizActivity extends ActionBarActivity {
         	@Override
         	public void onClick(View v) {
         	mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        	mIsCheater = false;
         	updateQuestion();
         	}
         	});
@@ -88,6 +96,19 @@ public class QuizActivity extends ActionBarActivity {
         	if(savedInstanceState != null){
         		mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         	}
+        	
+        	mCheatButton = (Button)findViewById(R.id.cheat_button);
+        	mCheatButton.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent i = new Intent(QuizActivity.this,CheatActivity.class);
+					boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+					i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+					startActivityForResult(i,0);			
+				}
+			});
+        	
         	
         updateQuestion();
 
@@ -130,16 +151,24 @@ public class QuizActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.quiz, menu);
         return true;
     }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		if(data == null){
+			return;
+		}
+		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
